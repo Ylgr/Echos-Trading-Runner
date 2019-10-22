29c4be63f1ebe0d5668fb5b9752b6a07faff6e2a
IMAC <>
2019-10-22 18:57:15 +0700

    add price emoji

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const moment = require('moment-timezone');
 const fs = require('fs');
 
-const tokenSymbol = 'BTC';
+const tokenSymbol = 'LINK';
 const withTokenSymbol = 'USDT';
 const apiKey = 'cdc591b1638966047a2fe76dd5b8460815685823fb0fcd9e2eb0cb71c34fb8f2';
 
@@ -29,6 +29,18 @@ function chunkArrayInGroups(arr, size) {
         myArray.push(arr.slice(i, i+size));
     }
     return myArray;
+}
+
+function executingPrice(price){
+    return price > 0.4 && price <= 1 ? '🙂' :
+    price > 1 && price <= 2 ? '😀' : 
+    price > 2 && price <= 4 ? '😆' :  
+    price > 4 ? '🤩' : 
+    price < -0.4 && price >= -1 ? '🙁' :
+    price < -1 && price >= -2 ? '😨' :
+    price < -2 && price >= -4 ? '😱' :
+    price < -4 ? '🥶' : 
+    '😐'
 }
 
 instance.get('https://min-api.cryptocompare.com/data/v2/histohour?fsym=' + tokenSymbol + '&tsym=' + withTokenSymbol + '&toTs=1571590800' )
@@ -59,7 +71,7 @@ instance.get('https://min-api.cryptocompare.com/data/v2/histohour?fsym=' + token
         const processedData = chunkArrayInGroups(preProcessedData, 24);
         const titleBar = '-,' + processedData[0].map(e=> e.time).join(',') + '\n';
         const content = processedData.map(dailyInWeek => {
-                return dailyInWeek[0].date + ',' + dailyInWeek.map(daily => daily.priceChange).join(',') + '\n'
+                return dailyInWeek[0].date + ',' + dailyInWeek.map(daily => executingPrice(daily.priceChange)).join(',') + '\n'
             }
         );
         fs.writeFile("result.csv", titleBar + content, function(err) {
